b3e51fb476930b043a49109e51643c72f79d8290
Matheus Barros <matheusbsousa95@gmail.com>
2019-08-01 23:23:54 -0300

    Alterações de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PacientesComponent} from './pacientes/pacientes.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {FeedbackComponent} from './feedback/feedback.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: PacientesComponent
+  },
+  {
+    path: '',
+    component: PacientesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'pacientes',
+    component: PacientesComponent
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { PacientesComponent } from './pacientes/pacientes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    PacientesComponent,
+    NotFoundComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
