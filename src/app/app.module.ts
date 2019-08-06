b6ed1c401a858f9f20cb9cdd150d053d1f50e934
Matheus Barros <matheusbsousa95@gmail.com>
2019-08-05 23:37:39 -0300

    Inserido feedback

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { PacientesComponent } from './pacientes/pacientes.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { FeedbackComponent } from './feedback/feedback.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent implements OnInit {
+model: FeedbackModel = {
+  titulo: '',
+  email: '',
+  descricao: ''
+};
+  private apiURL = 'http://localhost:8080/feedback';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() {
   }
 
+  envairFeedback(): void {
+    this.http.post(this.apiURL, this.model).subscribe(
+      res => {
+        location.reload();
+      },
+      err => {
+        alert('Ocorreu um erro ao enviar o feedback');
+      }
+    );
+  }
+
+}
+
+export interface FeedbackModel {
+  titulo: string;
+  email: string;
+  descricao: string;
 }
